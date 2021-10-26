039a1508d4ea6b2d6c01d4776196ac8e918f1aab
asd <asd>
2021-10-25 22:49:01 -0300

    asdasd

--- client/src/Timer.js
+++ client/src/Timer.js
@@ -5,8 +5,12 @@ const Timer = props => {
   const [time, setTime] = useState(60);
   useEffect(() => {
     const interval = setInterval(() => {
+      if (time > 0)
       setTime(prev => prev - 1);
+      else
+      setTime(0);
     }, 1000);
+
     return () => clearInterval(interval);
   });
 
